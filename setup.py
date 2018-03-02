f9365ac0b2314d1f5a93b926515346def6e8bb09
Piero Bardelli <pbardelli20@gmail.com>
2018-03-02 18:41:38 -0500

    Changed config extension from .yaml to .ini

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 def main():
     print("Setting up config file...")
-    with open("config.yml", "w") as file:
+    with open("config.ini", "w") as file:
         sg_api_key = input("Enter your SG API key: ")
-        file.writelines("sgconfig:")
-        file.writelines("\n\tsgkey: " + sg_api_key)
+        file.writelines("\n[SGCONFIG]\n")
+        file.writelines("SGKEY = " + sg_api_key + "\n")
 
 
 if __name__ == '__main__':
